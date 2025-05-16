441d6ac6cf15884109366b4230375b8e90614224
Niccolo-Rocchi <nik9991u@gmail.com>
2025-05-16 17:57:58 +0200

    Minor fixes

--- src/run.py
+++ src/run.py
@@ -6,7 +6,7 @@ import pandas as pd
 from scipy.stats import norm
 import pyagrum as gum
 
-from utils import *
+from src.utils import *
 
 warnings.filterwarnings('ignore')
 
@@ -23,8 +23,8 @@ def run_idm(conf):
     
     # Init local hyperp.
     exp = conf[0]
-    gpop_or = pd.read_csv(f"../data/{exp}.csv")
-    bn_or = gum.loadBN(f"../bns/{exp}.bif")
+    gpop_or = pd.read_csv(f"./data/{exp}.csv")
+    bn_or = gum.loadBN(f"./bns/{exp}.bif")
     n_nodes = len(bn_or.nodes())
     ess = conf[1].get("ess")
 
@@ -154,7 +154,7 @@ def run_idm(conf):
 
         except:
 
-            with open("../results/log.txt", "a") as log: 
+            with open("./results/log.txt", "a") as log: 
                 log.write(f"{exp}: error with sample {ds}.\n")
                 # log.write(traceback.format_exc())
 
